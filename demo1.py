9c73f4297e31312f30d7ced40cea11795103117d
ksralter <87271110+ksralter@users.noreply.github.com>
2021-07-23 23:49:06 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -3,9 +3,9 @@ from telethon import TelegramClient, events
 from telethon.tl.custom.message import Message
 from telethon.client.downloads import DownloadMethods
 from telethon.utils import get_input_location
-from pyaxmlparser import APK
+# from pyaxmlparser import APK
 import cryptg
-# from FastTelethonhelper import download_without_progressbar
+from FastTelethonhelper import download_without_progressbar
 
 api_id, api_hash = 936087, '5f6f172f2107d79ed3e2929075a78058'
 client = TelegramClient('telethon_session',api_id, api_hash)
